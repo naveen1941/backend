99ae3123db0865f7f8e8acfa7759dd656ca1f24e
Naveen Kumar Chittipolu <naveen.kumar@cafu.com>
2021-10-26 15:24:51 +0530

    ping service is added

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 const helloRoutes = require('./router/hello')
+const pingRoutes = require('./router/ping')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.use('/hello', helloRoutes)
+app.use('/ping', pingRoutes)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
