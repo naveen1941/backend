bf061f5c7e7f2fdfc6047f38905b1beeefec8da6
Naveen Kumar Chittipolu <naveen.kumar@cafu.com>
2021-10-26 15:54:33 +0530

    handle pagination aswell

--- router/sw.js
+++ router/sw.js
@@ -2,24 +2,38 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios').default;
 
-router.get('/chars-remote', function (req, res) {
+router.get('/chars-remote', async function (req, res) {
     // TODO: potential problems
     // external might go down
     // throttle..
     // url is hard-coded
     // code structure
-    axios.get('http://swapi.dev/api/people')
-        .then(function (response) {
-            if (response.status === 200) {
-                const resData = response.data.results.map( it => it.name)
-                res.send({msg: "success", names: resData});
-            } else {
-                return res.status(400).send({error: "Remote server is failed"})
-            }
-        })
-        .catch(function (error) {
+    // assert response format
+    let url = 'http://swapi.dev/api/people';
+    let resData = [];
+    while(url){
+        const resp = await swPaginationHelper(url)
+        if(resp.error) {
             return res.status(500).send({error: "Unknown error " + error})
-        })
+        }
+        resData = resData.concat(resp.data)
+        url = resp.next
+    }
+    res.send({msg: "success", names: resData});
 })
+
+async function swPaginationHelper(url) {
+    try {
+        const response = await axios.get(url)
+        if (response.status === 200) {
+            const resData = response.data.results.map( it => it.name)
+            return {data: resData, next: response.data.next}
+        } else {
+            return {error: new Error('External service call failed')}
+        }
+    } catch (error) {
+        return {error}
+    }
+}
 
 module.exports = router
